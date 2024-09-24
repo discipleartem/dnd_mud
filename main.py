a6aea48014d66068a20166c10373346561e76f49
Tomas <discipleartem@gmail.com>
2024-09-24 12:05:45 +0300

    Refactor constants and improve function naming
    Extracted constants for reuse and readability. Refactored function 'make_db' to 'load_database' for clarity. These changes improve code maintainability and readability.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
 import yaml  # Using PyYAML for YAML parsing
 
+# Extracted Constants
+DATABASE_FILE_PATH = 'database.yaml'
+WELCOME_MESSAGE = 'Добро пожаловать в текстовую игру по мотивам D&D 5й редакции!'
+
 
 def parse_yaml(file_path):
+    """Parse a YAML file and return the data."""
     with open(file_path, 'r', encoding='utf-8') as file:
         try:
             data = yaml.safe_load(file)
@@ -12,8 +17,8 @@ def parse_yaml(file_path):
     return data
 
 
-def make_db():
-    file_path = 'database.yaml'
+def load_database(file_path):
+    """Load the database from a YAML file."""
     if os.path.isfile(file_path):
         return parse_yaml(file_path)
     else:
@@ -21,19 +26,21 @@ def make_db():
         return {}
 
 
-GAME_DATA_BASE = make_db()
+# Inline the function call directly into the constant
+GAME_DATA_BASE = load_database(DATABASE_FILE_PATH)
 
 
 class Game:
-    WELCOME_MESSAGE = 'Добро пожаловать в текстовую игру по мотивам D&D 5й редакции!'
-
     @staticmethod
     def print_welcome_message():
-        print(Game.WELCOME_MESSAGE)
+        """Print the welcome message."""
+        print(WELCOME_MESSAGE)
 
     @staticmethod
     def run_game():
+        """Run the game."""
         Game.print_welcome_message()
 
 
+# Run the game
 Game.run_game()
