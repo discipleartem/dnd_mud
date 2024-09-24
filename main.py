a5fccdbffcae16908664e298f75ef67cb15df929
Tomas <discipleartem@gmail.com>
2024-09-24 23:21:44 +0300

    Refactor to use imported Messages class
    Replaced the inline Messages class with an import to improve modularity and maintainability. This change helps in reducing redundancy and centralizing message definitions.

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from yaml_parse import log_error, initialize_game_database
 from player import Player
+from massages import Messages
 from typing import Tuple, Dict, Any, Union, List
 from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class Messages:
-    WELCOME: str = 'Добро пожаловать в текстовую игру по мотивам D&D 5й редакции!'
-    GAME_ERROR: str = "Game cannot be started due to missing or corrupted database."
-    CHOOSE_RACE: str = "Выберите расу:"
-    INVALID_CHOICE: str = "Invalid choice or data error."
-    NO_RACES: str = "No available races to display."
-    ERROR_OCCURRED: str = "An error occurred: "
 
 
 @dataclass(frozen=True)
