fc5f4232a0832c92aa02f409b83feb6fd9441ca9
Tomas <discipleartem@gmail.com>
2024-09-25 00:02:09 +0300

    Add ErrorHandler class for logging and handling errors
    Introduced a new `ErrorHandler` class in `errors.py` to streamline error management. This class includes methods for logging messages and handling exceptions, ensuring consistency in error reporting across the application.

---
+++ errors.py
@@ -0,0 +1,18 @@
+from massages import Messages
+
+
+class ErrorHandler:
+    @staticmethod
+    def log_error(message: str) -> None:
+        """Handle error by printing a message."""
+        print(message)
+
+    @staticmethod
+    def log_error_message(message: str) -> None:
+        """Log an error message."""
+        ErrorHandler.log_error(message)  # Corrected the method call
+
+    @staticmethod
+    def handle_error(e: Exception) -> None:
+        """Handle errors."""
+        print(f"{Messages.ERROR_OCCURRED}{e}")
