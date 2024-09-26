87124163b813e2457bf2274170cb9f1fb578d972
Tomas <discipleartem@gmail.com>
2024-09-26 22:28:52 +0300

    Добавил присвоение особых свойств Рас(Human) к Player
    Add core mechanic for dice rolling and Human subclass
    
    Introduced a core mechanic to enable rolling a d20 dice within the game. Added a new Human subclass to manage player-specific attributes and functionalities, including a resourcefulness feature for re-rolling the dice. Finally, encapsulated player creation and race selection logic within the Game class.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from yaml_parse import initialize_game_database
 from dataclasses import dataclass
+from random import randint
 
 DATABASE = initialize_game_database()
 
@@ -15,8 +16,16 @@ class Messages:
     CHOOSE_RACE: str = CHOOSE_RACE_MESSAGE
 
 
+#Core mechanic can roll dice
 @dataclass
-class Creature:
+class Core:
+    @staticmethod
+    def roll_d20():
+        return randint(1, 20)
+
+
+@dataclass
+class Creature(Core):
     race: str
     race_name_ru: str
     creature_type: str
@@ -26,27 +35,56 @@ class Creature:
     size_name_ru: str
     speed: int
 
-
-# Player must be created from Race
 @dataclass
-class Player(Creature):
+class Human(Creature):
+    #Вы получаете Героическое вдохновение каждый раз после продолжительного отдыха
+    """ Героическое вдохновение (resourcefulness)
+       Если вы (Персонаж Игрока) имеете Героическое вдохновение, то вы можете потратить его, чтобы
+       перебросить любую кость сразу после броска, и вы обязаны использовать новый результат.
+       Если вы получаете Героическое вдохновение, но у вас оно уже есть, то оно теряется, если вы
+       не передадите его Персонажу Игрока, который не имеет его."""
+    resourcefulness :bool
+    resourcefulness_name_ru: str
+
     @staticmethod
-    def from_race_data(race_data: dict, race_key: str):
-        race_info = race_data[race_key]
-        return Player(
-            race=race_key,
-            race_name_ru=race_info['name']['ru'],
-            creature_type=race_info['creature_type']['type'],
-            creature_type_name_ru=race_info['creature_type']['name']['ru'],
-            description=race_info['description'],
-            size=race_info['size']['value'],
-            size_name_ru=race_info['size']['name']['ru'],
-            speed=race_info['speed']
+    def create_human_from_data(race_key, race_data: dict):
+        return Human(
+            race= race_key,
+            race_name_ru=race_data['name']['ru'],
+            creature_type=race_data['creature_type']['type'],
+            creature_type_name_ru=race_data['creature_type']['name']['ru'],
+            description=race_data['description'],
+            size=race_data['size']['value'],
+            size_name_ru=race_data['size']['name']['ru'],
+            speed=race_data['speed'],
+            resourcefulness=True if race_data['race_ability'].get('resourcefulness') else False,
+            resourcefulness_name_ru=race_data['race_ability']['resourcefulness']['name']['ru']
         )
 
+    def is_resourcefulness(self):
+        return self.resourcefulness
 
+    def roll_d20(self):
+        result = randint(1, 20)
+        print(result)
+        if self.is_resourcefulness():
+            print('Хотите перебросить кубик?')
+            print('1 - Да, 0 - Нет')
+            if int(input()):
+                result = result = randint(1, 20)
+                self.resourcefulness = False
+                return result
+
+
+
+# Player must be created from Race
+# Player can roll dice
+@dataclass
+class Player(Human):
+    pass
 
 #Game must run
+#Game has Core mechanic
 class Game:
 
     @staticmethod
@@ -66,18 +104,34 @@ class Game:
         return race_dict, race_keys
 
 
-    def run(self):
-        print(Messages.WELCOME)
-        print(Messages.CHOOSE_RACE)
+    @staticmethod
+    def create_player(race_key, race_data):
+        return Human.create_human_from_data(race_key, race_data)
 
-        race_dict, race_keys = self.create_race_dictionary(DATABASE[RACES_KEY])
+
+
+    def choose_race(self):
+        print(Messages.CHOOSE_RACE)
+        race_data = DATABASE[RACES_KEY]
+        race_dict, race_keys = self.create_race_dictionary(race_data)
         print(race_dict)
 
-        player_choice = int(input())
-        chosen_race_key = race_keys[player_choice]
+        chosen_race_index = int(input())
+        chosen_race_key = race_keys[chosen_race_index]
+        chosen_race_data = race_data[chosen_race_key]
+        return chosen_race_key, chosen_race_data
 
-        player = Player.from_race_data(DATABASE[RACES_KEY], chosen_race_key)
+    def run(self):
+        #Wellcome screen
+        print(Messages.WELCOME)
+
+        #choose race
+        race_key, race_data = self.choose_race()
+
+        #create player
+        player = self.create_player(race_key, race_data)
         print(player)
+        print(player.roll_d20())
 
 
 # Run the game
