6f3c5e2c1d1234552d98b5bc0c355783726e80da
Tomas <discipleartem@gmail.com>
2024-09-26 14:22:50 +0300

    Refactor race selection logic and player creation
    This update merges race selection directly into the game run method and adds functionality to create a player from the chosen race. Code comments and additional fields in data classes enhance clarity and localization support.

--- main.py
+++ main.py
@@ -12,28 +12,30 @@ class Messages:
 @dataclass
 class Creature:
     creature_type: str
+    creature_type_name_ru :str
+
     description: str
+
     size: str
+    size_name_ru: str
+
     speed: int
 
 @dataclass
 class Player(Creature):
     race: str
+    race_name_ru: str
 
 
 RACES_KEY = 'RACES' #ключ в котором содержаться все расы
 
 class Game:
 
-    def choose_race(self):
-        print(Messages.CHOOSE_RACE)
-        race_dict, race_keys = self.create_race_dictionary(DATABASE[RACES_KEY])
-        print(race_dict, race_keys)
-
     @staticmethod
     def create_race_dictionary(races):
         def is_valid_race(race):
             #Проверка наличия ключей, если их нет, то раса не создаеться.
+            # ['RACES']['name']['ru'] должно быть заполнено
             return race and race.get('name') and race['name'].get('ru')
 
         race_dict = {}
@@ -48,14 +50,38 @@ class Game:
 
         return race_dict, race_keys
 
+    @staticmethod
+    def create_player_from_race(race_data, race_key):
+        return Player(
+            creature_type=race_data[race_key]['creature_type']['type'],
+            creature_type_name_ru=race_data[race_key]['creature_type']['name']['ru'],
+            description=race_data[race_key]['description'],
+            size=race_data[race_key]['size']['value'],
+            size_name_ru=race_data[race_key]['size']['name']['ru'],
+            speed=race_data[race_key]['speed'],
+            race=race_key,
+            race_name_ru=race_data[race_key]['name']['ru']
+        )
+
 
     def run(self):
         print(Messages.WELCOME)
-        self.choose_race()
+        print(Messages.CHOOSE_RACE)
+
+        race_dict, race_keys = self.create_race_dictionary(DATABASE[RACES_KEY])
+        print(race_dict)
+
+        player_choice = int(input())
+        chosen_race_key = race_keys[player_choice]
+
+        player = self.create_player_from_race(DATABASE[RACES_KEY], chosen_race_key)
+        print(player)
+
+
 
 
 
 # Run the game
 if __name__ == "__main__":
     game_instance = Game()
-    game_instance.choose_race()
+    game_instance.run()
