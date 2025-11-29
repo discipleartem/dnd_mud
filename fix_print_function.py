bcd82efd697912f08aa1f974f91618957f44aa96
Tomas <discipleartem@gmail.com>
2025-11-29 08:06:34 +0200

    init commit

--- fix_print_function.py
+++
@@ -1,8 +0,0 @@
-import textwrap
-import shutil
-
-def wrap_print(text, width=None):
-    if width is None:
-        width, _ = shutil.get_terminal_size()
-    wrapped_text = textwrap.fill(text, width=width)
-    print(wrapped_text)
